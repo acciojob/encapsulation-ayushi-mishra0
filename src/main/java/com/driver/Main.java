9ed3470bc6ddacfd64b673d60f2f37410bb1c730
ayushi-mishra0 <msseema007@gmail.com>
2023-05-16 23:22:56 +0530

    initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,7 +1,20 @@
 package com.driver;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Ayushi");
+
+    public static void main(String args[]){
+        RWOnly obj = new RWOnly();
+        // Try setting a value to name by directly accessing it using obj
+       // obj.name = "John"; // Error: name has private access in RWOnly
+
+        // Try printing the value of name
+        //System.out.println(obj.name); // Error: name has private access in RWOnly
+
+        // Try setting a value to name using setter function
+        obj.setName("John");
+
+        // Try accessing the value of name using getter function
+        System.out.println(obj.getName()); // Output: John
     }
-}
+
+    }
